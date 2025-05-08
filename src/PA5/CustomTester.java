8710f9a2e894afdb108871d2a6d342cbee8b6257
Brian Masse <brianm25it@gmail.com>
2025-05-08 12:51:36 -0700

    added testing infastructure

--- src/PA5/CustomTester.java
+++ src/PA5/CustomTester.java
@@ -17,13 +17,114 @@ import org.junit.*;
  * The custom tester for PA5, which covers some basic test cases.
  */
 public class CustomTester {
-    // Optional: add test variables here
+
+    @FunctionalInterface
+    interface HashMapOperation {
+        void execute(MyHashMap map);
+    }
+
+    private MyHashMap.Node[][] storedHashMap;
+    private int storedHashSize;
+
+    private void printTranslatedHashMap(MyHashMap.Node[][] map) {
+        int length = map.length;
+        System.out.println("----");
+
+        for (int i = 0; i < length; i++) {
+            int j = 0;
+            String str = "[" + i + "]";
+
+            if (map[i] == null) {
+                System.out.println("[" + i + "] null");
+                continue;
+            }
+
+            while (map[i][j] != null) {
+                MyHashMap.Node node = map[i][j];
+                str += "[" + j + "] (Key: " + node.getKey() + ", Value: "
+                        + node.getValue() + "), ";
+
+                j++;
+
+            }
+            System.out.println(str);
+        }
+        System.out.println("----");
+
+    }
+
+    private MyHashMap.Node[][] translateHashMap(MyHashMap hashMap) {
+        int size = hashMap.size;
+        int capacity = hashMap.hashTable.length;
+        MyHashMap.Node[][] translatedMap = new MyHashMap.Node[capacity][size];
+
+        for (int i = 0; i < hashMap.hashTable.length; i++) {
+            MyHashMap.Node node = hashMap.hashTable[i];
+
+            if (node == null) {
+                translatedMap[i] = null;
+            } else {
+                MyHashMap.Node[] linkedList = new MyHashMap.Node[size];
+                int currentIndex = 0;
+
+                while (node != null) {
+
+                    linkedList[currentIndex] = node;
+
+                    node = node.getNext();
+                    currentIndex += 1;
+                }
+
+                translatedMap[i] = linkedList;
+            }
+        }
+
+        return translatedMap;
+    }
+
+    private void storeHashMap(MyHashMap map) {
+        this.storedHashMap = translateHashMap(map);
+        this.storedHashSize = map.size;
+    }
+
+    private void checkHashMap(MyHashMap map) {
+        MyHashMap.Node[][] translatedMap = this.translateHashMap(map);
+
+        assertEquals("Checking size", storedHashSize, map.size);
+        assertArrayEquals("Checking hashMapCorrectness", storedHashMap,
+                translatedMap);
+
+        int length = this.storedHashMap.length;
+        for (int i = 0; i < length; i++) {
+            String msg = "Checking LinkedList Correctness (" + i + ")";
+            assertArrayEquals(msg, this.storedHashMap[i], translatedMap[i]);
+        }
+    }
+
+    public void testExceptionRaised(MyHashMap map, Class<?> exceptionType,
+            HashMapOperation operation) {
+        boolean caughtException = false;
+        this.storeHashMap(map);
+
+        // check passing in an invalid param
+        try {
+            operation.execute(map);
+        } catch (Exception exception) {
+            if (exception.getClass() == exceptionType) {
+                caughtException = true;
+            }
+        }
+
+        assertTrue("Checking exception was Caught", caughtException);
+        this.checkHashMap(map);
+    }
 
     /**
-	 * This sets up the test fixture. JUnit invokes this method before
-	 * every testXXX method. The @Before tag tells JUnit to run this method
-	 * before each test.
-	 */
+     * This sets up the test fixture. JUnit invokes this method before every
+     * testXXX method. The @Before tag tells JUnit to run this method before
+     * each test.
+     */
+    // MARK: Before
     @Before
     public void setup() {
         // Optional: add setup here
@@ -31,14 +132,25 @@ public class CustomTester {
 
     // ----------------MyHashMap class----------------
 
+    // MARK: testConstructor
     /**
      * Test MyHashMap constructor with an invalid initial capacity
      */
     @Test
     public void testMyHashMapConstructorInvalidCapacity() {
-        // TODO: add your test here
+
+        MyHashMap map = new MyHashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("F", 3);
+
+        MyHashMap.printMap(map);
+
+        this.printTranslatedHashMap(translateHashMap(map));
+
     }
 
+    // MARK: testGetNullKey
     /**
      * Test MyHashMap get with a null key
      */
@@ -47,6 +159,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testGetKeyDoesNotExist
     /**
      * Test MyHashMap get with a key that does not exist in the hash table
      */
@@ -55,6 +168,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testPutNullKey
     /**
      * Test MyHashMap put with a null key
      */
@@ -63,6 +177,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testPutKeyAlreadyExists
     /**
      * Test MyHashMap put with a key that already exists in the hash table
      */
@@ -71,6 +186,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testRemoveNullKey
     /**
      * Test MyHashMap remove with a null key
      */
@@ -79,6 +195,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testRemoveKeyDoesNotExist
     /**
      * Test MyHashMap remove with a key that does not exist in the hash table
      */
@@ -87,6 +204,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testGetHashNullKey
     /**
      * Test MyHashMap getHash with a null key
      */
@@ -97,6 +215,7 @@ public class CustomTester {
 
     // ----------------MyHashSet class----------------
 
+    // MARK: testAddAlreadyExists
     /**
      * Test MyHashMap put with a key that already exists in the hash table
      */
@@ -105,6 +224,7 @@ public class CustomTester {
         // TODO: add your test here
     }
 
+    // MARK: testRemoveDoesNotExist
     /**
      * Test MyHashSet remove with a key that does not exist in the hash table
      */
